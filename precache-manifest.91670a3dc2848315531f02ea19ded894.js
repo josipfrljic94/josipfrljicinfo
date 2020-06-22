6d934b31ac8f767f81bcbb36b745410550b18d27
josip frljic <josipfrljic77@gmail.com>
2020-06-22 12:37:47 +0200

    Updates

---
+++ precache-manifest.91670a3dc2848315531f02ea19ded894.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05dc6712c479f3305dc78ca8f35d8c4",
+    "url": "/josipfrljicinfo/index.html"
+  },
+  {
+    "revision": "418b2bf0c37575673a16",
+    "url": "/josipfrljicinfo/static/css/main.d7425d3f.chunk.css"
+  },
+  {
+    "revision": "764aa7a72bd4b1f831e0",
+    "url": "/josipfrljicinfo/static/js/2.72fff715.chunk.js"
+  },
+  {
+    "revision": "4d1cd13d06270bdc644d7e525347624e",
+    "url": "/josipfrljicinfo/static/js/2.72fff715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cc14058bbb082038b88",
+    "url": "/josipfrljicinfo/static/js/3.b66748b6.chunk.js"
+  },
+  {
+    "revision": "b0aea892ccb904d9650f",
+    "url": "/josipfrljicinfo/static/js/4.f4fea6eb.chunk.js"
+  },
+  {
+    "revision": "3c2fc00b22a82bdbd032",
+    "url": "/josipfrljicinfo/static/js/5.133368b0.chunk.js"
+  },
+  {
+    "revision": "418b2bf0c37575673a16",
+    "url": "/josipfrljicinfo/static/js/main.b3c8ea95.chunk.js"
+  },
+  {
+    "revision": "1faf90247bc0c618269f",
+    "url": "/josipfrljicinfo/static/js/runtime-main.f48ae814.js"
+  },
+  {
+    "revision": "22cf12f73ca3910180c6d45c0302f3ab",
+    "url": "/josipfrljicinfo/static/media/carshop1od2.22cf12f7.png"
+  },
+  {
+    "revision": "d7712a9ceeb3396f262c0449c288c6a2",
+    "url": "/josipfrljicinfo/static/media/carshop2od2.d7712a9c.png"
+  },
+  {
+    "revision": "d5692a6dbcad5760063ab8b1db13870a",
+    "url": "/josipfrljicinfo/static/media/job.d5692a6d.jpg"
+  },
+  {
+    "revision": "28ae75d07b4aa93588ed06534e077838",
+    "url": "/josipfrljicinfo/static/media/josip.28ae75d0.png"
+  },
+  {
+    "revision": "d79d184b8a7fd8f7830453fc388331b3",
+    "url": "/josipfrljicinfo/static/media/milijunas1od2.d79d184b.png"
+  },
+  {
+    "revision": "2c31665e207f153769a76182ce5848b1",
+    "url": "/josipfrljicinfo/static/media/milijunas2od2.2c31665e.png"
+  },
+  {
+    "revision": "fd69b947e74a85a7f94928297c3e2531",
+    "url": "/josipfrljicinfo/static/media/person-in-front-of-laptop-on-brown-wooden-table-2115217 (1).fd69b947.jpg"
+  },
+  {
+    "revision": "abca5402faf5a4af9ff9de01bcab0b87",
+    "url": "/josipfrljicinfo/static/media/photo-of-person-playing-acoustic-guitar-1751731.abca5402.jpg"
+  },
+  {
+    "revision": "a803b7b93d8f72b981268c16905eaf81",
+    "url": "/josipfrljicinfo/static/media/screen1od2.a803b7b9.png"
+  },
+  {
+    "revision": "f319b7ea34c688a13f0a7411ddb06528",
+    "url": "/josipfrljicinfo/static/media/screen2.f319b7ea.png"
+  },
+  {
+    "revision": "dd0711b572bc11d2a25cbb4097de0562",
+    "url": "/josipfrljicinfo/static/media/theatre.dd0711b5.jpg"
+  },
+  {
+    "revision": "e9c08dfc7e282c77e390b3f4b65f1334",
+    "url": "/josipfrljicinfo/static/media/woman-wearing-white-karati-g-under-blue-sky-3023756.e9c08dfc.jpg"
+  }
+]);
